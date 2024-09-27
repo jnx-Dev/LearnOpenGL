7403b5fc6604f82e5cc41200ada5d3ad1824e56e
Jonas <jonas.merker@smail.inf.h-brs.de>
2024-09-27 14:14:43 +0200

    finished exercise 1.2

--- LearnOpenGL/src/main.cpp
+++ LearnOpenGL/src/main.cpp
@@ -108,19 +108,23 @@ int main () {
     float vertices[] = {
      0.5f,  0.5f, 0.0f,  // top right
      0.5f, -0.5f, 0.0f,  // bottom right
+    -0.5f,  0.5f, 0.0f   // top left 
+    };
+    float verticesSec[] = {
+     0.5f, -0.5f, 0.0f,  // bottom right
     -0.5f, -0.5f, 0.0f,  // bottom left
     -0.5f,  0.5f, 0.0f   // top left 
     };
-    unsigned int indices[] = {  // note that we start from 0!
+    /*unsigned int indices[] = {
         0, 1, 3,   // first triangle
         1, 2, 3    // second triangle
-    };
+    };*/
 
     // Buffers
-    unsigned int VBO, VAO, EBO;
+    unsigned int VBO, VAO/*, EBO*/;
     glGenVertexArrays(1, &VAO);
     glGenBuffers(1, &VBO);
-    glGenBuffers(1, &EBO);
+    //glGenBuffers(1, &EBO);
 
     glBindVertexArray(VAO);
 
@@ -128,13 +132,27 @@ int main () {
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    //glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    //glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     // Set vertex attributes pointers
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
+#pragma region Second
+    unsigned int VBOsec, VAOsec;
+
+    glGenVertexArrays(1, &VAOsec);
+    glGenBuffers(1, &VBOsec);
+
+    glBindVertexArray(VAOsec);
+
+    glBindBuffer(GL_ARRAY_BUFFER, VBOsec);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(verticesSec), verticesSec, GL_STATIC_DRAW); 
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+#pragma endregion
 
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -144,7 +162,7 @@ int main () {
     glBindVertexArray(0);
 
     // uncomment this call to draw in wireframe polygons.
-    //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
     // render loop
     // -----------
@@ -160,7 +178,9 @@ int main () {
         // Drawing
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(VAOsec);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
         // glBindVertexArray(0); // no need to unbind it every time 
 
         // Check and call events and swap the buffers
